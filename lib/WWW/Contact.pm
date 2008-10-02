65856a1acbc19c2086049229b0ad052e30cb9581
Fayland 林 <fayland@gmail.com>
2008-10-02 02:40:26 +0000

    Import of FAYLAND/WWW-Contact-0.01 from CPAN.
    gitpan-cpan-distribution: WWW-Contact
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         FAYLAND/WWW-Contact-0.01.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/Contact.pm
@@ -0,0 +1,167 @@
+package WWW::Contact;
+
+use vars qw/$VERSION $AUTHORITY/;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+$VERSION   = '0.01';
+$AUTHORITY = 'cpan:FAYLAND';
+
+has 'errstr'   => ( is => 'rw', isa => 'Maybe[Str]' );
+has 'supplier' => ( is => 'rw', isa => 'Str' );
+has 'custom_supplier_code' => ( is => 'rw', isa => 'CodeRef', default => sub { sub {} } );
+
+sub get_contacts {
+    my $self = shift;
+    my ( $email, $password ) = @_;
+    
+    unless ( $email and $password ) {
+        $self->errstr('Both email and password are required.');
+        return;
+    }
+    
+    unless ( $email =~ m/^(.+)\@(([^.]+)\.(.+))$/ ) {
+        $self->errstr('You must supply full email address.');
+        return;
+    }
+    
+    my ( $username, $postfix ) = ( lc($1), lc($2) );
+    
+    # get supplier module
+    unless ($self->supplier) {
+        $self->get_supplier_by_email($email) or $self->custom_supplier_code($email);
+        unless ($self->supplier) {
+            $self->errstr("$email is not supported yet.");
+            return;
+        }
+    }
+    
+    my $module = 'WWW::Contact::' . $self->supplier;
+    eval("use $module;");
+    if ($@) {
+        $self->errstr($@);
+        return;
+    }
+    
+    my $wc = new $module;
+    
+    # reset
+    $self->errstr(undef);
+    
+    my $contacts = $wc->get_contacts( $email, $password );
+
+    if ( $wc->errstr ) {
+        $self->errstr( $wc->errstr );
+        return;
+    } else {
+        return wantarray ? @$contacts : $contacts;
+    }
+}
+
+sub get_supplier_by_email {
+    my ($self, $email) = @_;
+    
+    if ($email =~ /[\@\.]yahoo\./) {
+        $self->supplier('Yahoo');
+    } elsif ($email =~ /\@gmail\.com$/) {
+        $self->supplier('Gmail');
+    }
+    
+    return $self->supplier ? 1 : 0;
+}
+
+no Moose;
+no Moose::Util::TypeConstraints;
+
+1;
+__END__
+
+=head1 NAME
+
+WWW::Contact - Get contacts/addressbook from Web
+
+=head1 SYNOPSIS
+
+    use WWW::Contact;
+    
+    my $wc       = WWW::Contact->new();
+    my @contacts = $wc->get_contacts('fayland@gmail.com', 'password');
+    my $errstr   = $wc->errstr;
+    if ($errstr) {
+        die $errstr; # like 'Wrong Password'
+    } else {
+        print Dumper(\@contacts);
+    }
+
+=head1 DESCRIPTION
+
+Get Contacts/AddressBook from public websites.
+
+=head1 SUPPORTED EMAIL SUPPLIER
+
+=over 4
+
+=item Gmail
+
+=item Yahoo! Mail
+
+=back
+
+=head1 HOW TO WRITE MY OWN MODULE
+
+please read L<WWW::Contact::Base> and examples: L<WWW::Contact::Yahoo> and L<WWW::Contact::Gmail>
+
+Assuming we write a custom module as WWW::Contact::Unknown
+
+    package WWW::Contact::Unknown;
+    
+    use Moose;
+    extends 'WWW::Contact::Base';
+    
+    sub get_contacts {
+        my ($self, $email, $password) = @_;
+        
+        # reset
+        $self->errstr(undef);
+        
+        if ($email eq 'a@a.com' and $password ne 'a') {
+            $self->errstr('Wrong Password');
+            return;
+        }
+        
+        my @contacts = ( {
+            email => 'b@b.com',
+            name => 'b',
+        }, {
+            email => 'c@c.com',
+            name => 'c'
+        } );
+        return wantarray ? @contacts : \@contacts;
+    }
+    
+    1;
+
+We can use it within WWW::Contact
+
+    my $wc = new WWW::Contact;
+    $wc->supplier('Unknown'); # it will eval use WWW::Contact::Unknown when get_contacts
+    
+    my @contacts = $wc->get_contacts('a@a.com', 'b');
+    my $errstr = $wc->errstr;
+
+=head1 SEE ALSO
+
+L<WWW::Contact::Gmail>, L<WWW::Contact::Yahoo>, L<WWW::Mechanize>, L<Moose>
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
---
+++ lib/WWW/Contact/Yahoo.pm
@@ -0,0 +1,123 @@
+package WWW::Contact::Yahoo;
+
+use Moose;
+use WWW::Mechanize::GZip;
+
+extends 'WWW::Contact::Base';
+
+sub BUILD {
+    my ($self) = @_;
+    
+    $self->{ua} = WWW::Mechanize::GZip->new(
+        agent       => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+        cookie_jar  => {},
+        stack_depth => 1,
+    );
+}
+
+sub get_contacts {
+    my ($self, $email, $password) = @_;
+
+    # reset
+    $self->errstr(undef);
+    my @contacts;
+    
+    my $ua = $self->ua;
+    $self->debug("start get_contacts from Yahoo!");
+    
+    # to form
+    $self->get('https://login.yahoo.com/config/login_verify2?&.src=ym') || return;
+    $self->submit_form(
+        form_name => 'login_form',
+        fields    => {
+            login  => $email,
+            passwd => $password,
+        },
+    ) || return;
+    my $content = $ua->content();
+    if ($content =~ /=[\'\"]yregertxt/) {
+        $self->errstr('Wrong Password');
+        return;
+    }
+    
+    $self->debug('Login OK');
+
+    $self->get('http://address.mail.yahoo.com/') || return;
+    $ua->follow_link( url_regex => qr/import_export/i );
+    
+    ##### detect the export form, the last form
+    my $form_number = 3;
+    while ( $form_number > 0 && !$self->{ua}->form_number($form_number) ) {
+        $form_number--;
+    }
+    #####
+
+    eval {
+        $self->submit_form(
+            form_number => $form_number,
+            button      => 'submit[action_export_yahoo]',
+        );
+    };
+    
+    $content = $ua->content();
+    my $i;
+    while ( $content
+        =~ /^\"(.*?)\"\,\".*?\"\,\"(.*?)\"\,\".*?\"\,\"(.*?)\"\,\".*?\"\,\".*?\"\,\"(.*?)\"/mg
+        ) {
+        $i++;
+        next if ( $i == 1 );    # skip the first line.
+        next unless ( $3 or $4 );
+        my $email = $3 || $4 . '@yahoo.com';
+        my $name = ( $1 or $2 ) ? "$1 $2" : $4;
+        $_ = {
+            name       => $name,
+            email      => $email,
+        };
+        push @contacts, $_;
+    }
+
+    return wantarray ? @contacts : \@contacts;
+}
+
+no Moose;
+
+1;
+__END__
+
+=head1 NAME
+
+WWW::Contact::Yahoo - Get contacts/addressbook from Yahoo! Mail
+
+=head1 SYNOPSIS
+
+    use WWW::Contact::Yahoo;
+    
+    my $wc       = WWW::Contact::Yahoo->new();
+    my @contacts = $wc->get_contacts('itsa@yahoo.com', 'password');
+    my $errstr   = $wc->errstr;
+    if ($errstr) {
+        die $errstr;
+    } else {
+        print Dumper(\@contacts);
+    }
+
+=head1 DESCRIPTION
+
+get addressbook from Yahoo! Mail. extends L<WWW::Contact::Base>
+
+=head1 SEE ALSO
+
+L<WWW::Contact>, L<WWW::Mechanize::GZip>
+
+=head1 AUTHOR
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Fayland Lam, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
