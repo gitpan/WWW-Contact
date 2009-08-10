98a02c45dbadd9605e653324a8e790aeb3603477
Fayland 林 <fayland@gmail.com>
2009-08-10 05:45:46 +0000

    Import of FAYLAND/WWW-Contact-0.28 from CPAN.
    gitpan-cpan-distribution: WWW-Contact
    gitpan-cpan-version:      0.28
    gitpan-cpan-path:         FAYLAND/WWW-Contact-0.28.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/WWW/Contact.pm
+++ lib/WWW/Contact.pm
@@ -3,7 +3,7 @@ package WWW::Contact;
 use Class::MOP ();
 use Moose;
 
-our $VERSION   = '0.27';
+our $VERSION   = '0.28';
 our $AUTHORITY = 'cpan:FAYLAND';
 
 has 'errstr'   => ( is => 'rw', isa => 'Maybe[Str]' );
